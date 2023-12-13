4a8431c41dd31113b5e979bbd861da29bdd3a102
Carlos Veron <carloseduardo644@gmail.com>
2023-12-13 00:41:51 -0300

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:marvel/main.dart';
+import 'package:marvel/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
